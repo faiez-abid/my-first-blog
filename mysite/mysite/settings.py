353375e1c0e28dac813a39cb06625de01acbdd22
faiez <faiez.abid@softcatalyst.com>
2019-07-11 12:30:03 +0100

    Added view and template for detailed blog post as well as CSS for the site.

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -25,8 +25,7 @@ SECRET_KEY = 'ysdl8^7r+($t&=4xt@8ehba8r9_*)$%%$71fp&#ed=ev2eoi9r'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 # Application definition
 
@@ -119,3 +118,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
